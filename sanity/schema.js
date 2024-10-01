f03d488dc051758d05bf1622aff8ae0666e11e12
Tore Marius Akerbæk <marius.akerbak@gmail.com>
2024-10-01 15:53:49 +0200

    Fikset forsidevideoer

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,7 +2,8 @@ import { album } from "./schemas/albums";
 import { song } from "./schemas/songs";
 import blockContent from "./schemas/blockContent";
 import fieldset from "./schemas/objects/fieldset";
+import { fpvideo } from "./schemas/frontpageVideo";
 
 export const schema = {
-  types: [album, song, blockContent, fieldset],
+  types: [album, song, blockContent, fieldset, fpvideo],
 }
